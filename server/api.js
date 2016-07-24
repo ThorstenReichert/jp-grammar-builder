a970b098ee0c6ef268740f0f1c91a608e8ff8e23
Thorsten Reichert <konsort@gmx.de>
2016-07-24 15:01:26 +0200

    use wagner-core for DI

--- server/api.js
+++ server/api.js
@@ -3,25 +3,28 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const router = express.Router();
-const parseKana = require('./middleware/parse-kana');
-const procKana = require('./middleware/proc-kana');
 
-router.get('/ping', function (req, res) {
-    res.status(200).send();
-});
+module.exports = function (wagner) {
+    const parseKana = require('./middleware/parse-kana')(wagner);
+    const procKana = require('./middleware/proc-kana')(wagner);
 
-router.all('/kana',
-    bodyParser.urlencoded({extended: false}),
-    bodyParser.json(),
-    parseKana,
-    procKana,
-    function (req, res) {
-        res.status(200).json(res.kana);
-    }
-);
+    router.get('/ping', function (req, res) {
+        res.status(200).send();
+    });
 
-router.all('*', function (req, res) {
-    res.status(404).send();
-});
+    router.all('/kana',
+        bodyParser.urlencoded({extended: false}),
+        bodyParser.json(),
+        parseKana,
+        procKana,
+        function (req, res) {
+            res.status(200).json(req.kana);
+        }
+    );
 
-module.exports = router;
+    router.all('*', function (req, res) {
+        res.status(404).send();
+    });
+
+    return router;
+}
--- server/worker.js
+++ server/worker.js
@@ -4,12 +4,19 @@ const cluster = require('cluster');
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const wagner = require('wagner-core');
 
 const app = express();
 
 // config
 const port = process.env.NODE_PORT || 3000;
 const id = cluster.worker.id;
+
+// setup DI
+const kana = require('../kana');
+wagner.factory('kana', function () {
+    return kana;
+});
 
 // setup logging
 morgan.token('id', function getId(req) {
@@ -23,7 +30,7 @@ app.use('/client', express.static(
 ));
 
 // setup api
-app.use('/api', require('./api'));
+app.use('/api', require('./api')(wagner));
 
 // disconnect worker on uncaught exception
 // prompts forky to restart worker
