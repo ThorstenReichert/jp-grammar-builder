ba07f67379b0ced9ed56f1e890ddc393c0f1be47
Thorsten (Gerbrunn) <th.reichert@gmx.net>
2016-08-29 17:06:14 +0200

    use path.join

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 'use strict';
 
 const forky = require('forky');
+const path = require('path');
 
 forky({
-    path: __dirname + '/worker.js',
+    path: path.join(__dirname, '/worker.js'),
     workers: process.env.WEB_CONCURRENCY || 1,
     enable_logging: true
 });
