d02d690b2fb380dde65688cc413ced5ee65bbc18
Thorsten (Gerbrunn) <th.reichert@gmx.net>
2016-08-31 15:23:20 +0200

    use winston for logging

--- server/worker.js
+++ server/worker.js
@@ -8,18 +8,13 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const path = require('path');
 const wagner = require('wagner-core');
+const winston = require('winston');
 
 const app = express();
 
 // config
 const port = process.env.NODE_PORT || 3000;
 const id = cluster.worker.id;
-
-// setup DI
-const kana = require('../kana');
-wagner.factory('kana', function () {
-    return kana;
-});
 
 // setup memory leak detection
 const memwatch = require('memwatch-next');
@@ -37,11 +32,49 @@ memwatch.on('leak', function (info) {
     });
 });
 
-memwatch.on('stats', function (stats) {
-    console.log(stats);
+// setup logging
+const logger = new winston.Logger({
+    transports: [
+        new (winston.transports.Console)({
+            name: 'console',
+            level: 'debug',
+            formatter: function (options) {
+                return id + ' | ' + options.level + ': ' +
+                    (undefined !== options.message ? options.message : '') +
+                    (options.meta && Object.keys(options.meta).length ? '\n\t'+ JSON.stringify(options.meta) : '' );
+            }
+        }),
+        new (winston.transports.File)({
+            name: 'file',
+            level: 'warn',
+            filename: path.join(__dirname, '/log/all-logs.log'),
+            timestamp: true
+        })
+    ],
+    exceptionHandlers: [
+        new (winston.transports.Console)({
+            humanReadableUnhandledException: true
+        }),
+        new (winston.transports.File)({
+            filename: path.join(__dirname, '/log/exceptions.log')
+        })
+    ]
+});
+logger.info('setup logging');
+
+// setup DI
+logger.info('setup wagner');
+const kana = require('../kana');
+wagner.factory('kana', function () {
+    return kana;
+});
+
+wagner.factory('logger', function () {
+    return logger;
 });
 
 // setup helmet
+logger.info('setup helmet');
 app.use(helmet.xssFilter());
 app.use(helmet.frameguard({ action: 'deny' }));
 app.use(helmet.hidePoweredBy());
@@ -49,25 +82,69 @@ app.use(helmet.ieNoOpen());
 app.use(helmet.noSniff());
 
 // setup compression
+logger.info('setup compression');
 app.use(compression());
 
 // setup favicon
+logger.info('setup favicon');
 app.use(favicon(
     path.join(__dirname, '../client/flag.ico')
 ));
 
 // setup client
+logger.info('setup client');
 app.use('/client', express.static(
     path.join(__dirname, '/../client')
 ));
 
-// setup logging (ignore client requests)
-morgan.token('id', function getId(req) {
-    return id;
+// setup morgan (ignore client requests)
+logger.info('setup morgan');
+const connectionCombinedLogger = new winston.Logger({
+    transports: [
+        new (winston.transports.File)({
+            level: 'info',
+            filename: path.join(__dirname, '/log/connections.log'),
+            json: true,
+            maxsize: 5242880, //5MB
+            maxFiles: 5,
+            colorize: false
+        })
+    ]
 });
-app.use(morgan(':id | :remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length]'));
 
-// setup api
+connectionCombinedLogger.stream = {
+    write: function (message, encoding) {
+        connectionCombinedLogger.info(message);
+    }
+};
+
+const connectionDevLogger = new winston.Logger({
+    transports: [
+        new (winston.transports.Console)({
+            level: 'debug',
+            handleExceptions: true,
+            json: false,
+            colorize: true,
+            formatter: function (options) {
+                return id + ' | ' + options.level + ': ' +
+                    (undefined !== options.message ? options.message : '') +
+                    (options.meta && Object.keys(options.meta).length ? '\n\t'+ JSON.stringify(options.meta) : '' );
+            }
+        })
+    ]
+});
+
+connectionDevLogger.stream = {
+    write: function (message, encoding) {
+        connectionDevLogger.info(message);
+    }
+};
+
+app.use(morgan('combined', { 'stream': connectionCombinedLogger.stream }));
+app.use(morgan('dev', { 'stream': connectionDevLogger.stream }));
+
+// setup api router
+logger.info('setup api router');
 app.use('/api', require('./api')(wagner));
 
 // default to /client
@@ -78,11 +155,11 @@ app.use('/', function (req, res, err) {
 // disconnect worker on uncaught exception
 // prompts forky to restart worker
 process.on('uncaughtException', function (err) {
-    console.error(err);
+    logger.error(err);
     require('forky').disconnect();
 });
 
 // start server
 app.listen(port, function () {
-    console.log(id + ' | listening on port ' + port);
+    logger.info('listening on port ' + port);
 });
