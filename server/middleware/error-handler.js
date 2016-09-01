c7c104b32e2b53bd509483de4fafb6737793efff
Thorsten (Gerbrunn) <th.reichert@gmx.net>
2016-09-01 18:37:39 +0200

    strip stacktrace if not in development

--- server/middleware/error-handler.js
+++ server/middleware/error-handler.js
@@ -1,9 +1,10 @@
 'use strict';
 
 const httpStatus = require('http-status');
+const env = require('../config/env');
 
 module.exports = function (wagner) {
-    return wagner.invoke(function (logger) {
+    return wagner.invoke(function (config, logger) {
 
         return function (err, req, res, next) {
             if (!err) {
@@ -11,6 +12,11 @@ module.exports = function (wagner) {
             }
 
             logger.error(err);
+
+            if (config.env !== env.development) {
+                delete err.stack;
+            }
+
             return res.status(httpStatus.INTERNAL_SERVER_ERROR).json(err);
         };
 
