4f4030ca3a660308e86960339e526846fd12fa6f
Thorsten (Gerbrunn) <th.reichert@gmx.net>
2016-11-09 21:11:49 +0100

    added romanization info to kana input

--- client/src/resources/elements/kana-input.js
+++ client/src/resources/elements/kana-input.js
@@ -1,9 +1,12 @@
-import {bindable, inject} from 'aurelia-framework';
+import {bindable, inject, LogManager} from 'aurelia-framework';
 import {observable} from 'aurelia-binding';
+import $ from 'bootstrap';
 import {KanaService} from '../services/kana';
 import {GrammarService} from '../services/grammar';
 import {rules} from '../constants/rules';
 import {types} from '../constants/types';
+
+const log = LogManager.getLogger('KanaInput');
 
 @inject(KanaService, GrammarService)
 export class KanaInput {
@@ -23,6 +26,11 @@ export class KanaInput {
   @observable({changeHandler: 'onTypeChange'}) type = '';
   onTypeChange(newValue, oldValue) {
     this.KanaService.type = newValue;
+  }
+
+  attached() {
+    log.debug('enabling popovers');
+    $('[data-toggle="popover"]').popover({html:true});
   }
 }
 
